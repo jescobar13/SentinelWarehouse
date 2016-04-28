9a98d132e9a45251fd25a7806a1491c8f2666d62
Jordi Escobar <bustiadenjordi@gmail.com>
2016-04-28 11:21:21 +0200

    Creacio Estructura Producte
    S'ha creat l'estructura en el projecte de Producte, Creació dels fitxers
    vista, control, model.

---
+++ WarehouseSentinel/Models/TProducte.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseSentinel.Models
+{
+    public class TProducte
+    {
+        private SentinelDBEntities context;
+
+        public TProducte(SentinelDBEntities context)
+        {
+            this.context = context;
+        }
+
+        public void add(producte p)
+        {
+            context.producte.Add(p);
+            context.SaveChanges();
+        }
+
+        public void modify(producte p)
+        {
+            context.SaveChanges();
+        }
+
+        public void remove(producte p)
+        {
+            context.producte.Remove(p);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ WarehouseSentinel/Viwers/Producta/ProducteWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WarehouseSentinel.Viwers.Producta
+{
+    /// <summary>
+    /// Lógica de interacción para ProducteWindow.xaml
+    /// </summary>
+    public partial class ProducteWindow : Window
+    {
+        public ProducteWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
